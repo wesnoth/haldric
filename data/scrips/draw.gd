812cc41e99d81d5e5519ab34eaa76897dcb15e1e
Aaron Winter <winter.aaron93@gmail.com>
2018-09-30 16:45:48 +0200

    removed filter from textures, added tile dictionary

--- data/scrips/draw.gd
+++ data/scrips/draw.gd
@@ -1,9 +1,12 @@
 extends Control
 
-onready var map = $"../../Bottom"
+onready var map = $"../../TileMap"
 onready var selector = $"Selector"
 
 func _process(delta):
 	var cell = map.world_to_map(get_global_mouse_position())
 	selector.position = map.map_to_world(cell)
-	$Selector/TileCoords.text = str(cell)
+	if map.check_boundaries(cell):
+		$Selector/TileCoords.text = str(map.tiles[map.flatten_v(cell)].cell)
+		$Selector/TileType.text = str(map.tiles[map.flatten_v(cell)].type)
+		
---
+++ data/scrips/map.gd
@@ -0,0 +1,38 @@
+extends TileMap
+
+export (int) var WIDTH = 20
+export (int) var HEIGHT = 10
+
+var tiles = {}
+
+func _ready():
+	_generate_tiles()
+
+
+# P U B L I C   F U N C T I O N S
+
+
+func flatten_v(_cell):
+	return int(_cell.y) * WIDTH + int(_cell.x)
+
+func flatten(_x, _y):
+	return _y * WIDTH + _x
+
+func check_boundaries(_point):
+	return (_point.x >= 0 and _point.y >= 0 and _point.x < WIDTH  and _point.y < HEIGHT)
+
+
+# P R I V A T E   F U N C T I O N S
+
+
+func _generate_tiles():
+	for y in range(HEIGHT):
+		for x in range(WIDTH):
+			var cell = Vector2(x, y)
+			var id = flatten_v(cell)
+			tiles[id] = {
+					cell = cell,
+					type = get_cell(cell.x, cell.y),
+					weight = 1,
+					is_blocked = false,
+			}
