97b32e5e9c266043e4d73467e300b042075e72f4
Aaron Winter <winter.aaron93@gmail.com>
2018-10-01 17:51:55 +0200

    added 'G' key for toggling the A* connection grid on and off

--- data/scripts/interface.gd
+++ data/scripts/interface.gd
@@ -3,6 +3,12 @@ extends Node2D
 onready var cursor = $Cursor
 onready var terrain = $"../Terrain"
 
+var show_grid = false
+
+func _input(event):
+	if event and Input.is_action_just_pressed("toggle_grid"):
+		show_grid = !show_grid
+
 func _process(delta):
 	update()
 	cursor.position = terrain.world_to_world_centered(get_global_mouse_position())
@@ -17,17 +23,17 @@ func _draw():
 	for n in range(terrain.path.size()):
 		draw_circle(terrain.map_to_world_centered(terrain.path[n]), 5, Color(255, 0, 0))
 	
-#	# DRAW CONNECTIONS
-#	var points = terrain.grid.get_points()
-#	for p in points:
-#		var p_pos = terrain.grid.get_point_position(p) * Vector3(54, 72, 0) + Vector3(36, 36, 0)
-#		if int(terrain.tiles[p].cell.x) % 2 == 1:
-#			p_pos += Vector3(0, 36, 0)
-#		var neighbors = terrain.grid.get_point_connections(p)
-#		for n in neighbors:
-#			var n_cell = terrain.grid.get_point_position(n)
-#			var n_pos = n_cell * Vector3(54, 72, 0) + Vector3(36, 36, 0)
-#			if int(n_cell.x) & 1 == 1:
-#				print(n_cell.x)
-#				n_pos += Vector3(0, 36, 0)
-#			draw_line(Vector2(p_pos.x, p_pos.y), Vector2(n_pos.x, n_pos.y), Color(255, 0, 0), 1)
+	# DRAW CONNECTIONS
+	if show_grid:
+		var points = terrain.grid.get_points()
+		for p in points:
+			var p_pos = terrain.grid.get_point_position(p) * Vector3(54, 72, 0) + Vector3(36, 36, 0)
+			if int(terrain.tiles[p].cell.x) % 2 == 1:
+				p_pos += Vector3(0, 36, 0)
+			var neighbors = terrain.grid.get_point_connections(p)
+			for n in neighbors:
+				var n_cell = terrain.grid.get_point_position(n)
+				var n_pos = n_cell * Vector3(54, 72, 0) + Vector3(36, 36, 0)
+				if int(n_cell.x) & 1 == 1:
+					n_pos += Vector3(0, 36, 0)
+				draw_line(Vector2(p_pos.x, p_pos.y), Vector2(n_pos.x, n_pos.y), Color(255, 0, 0), 1)
