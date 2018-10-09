e59d8960f482cf09840913156dac1d1efbfc67da
Charles Dang <exodia339@gmail.com>
2018-10-09 14:22:53 +1100

    Set smooth camera via the editor instead

--- interface/WesnothCamera.gd
+++ interface/WesnothCamera.gd
@@ -3,28 +3,24 @@ extends Camera2D
 var speed = 2000
 var border = 4
 
-func _ready():
-	smoothing_enabled = true
-	smoothing_speed = 5
-
 func _input(event):
-	
+
 	if Input.is_action_just_pressed("scroll_up"):
 		position.y -= 100
-	
+
 	if Input.is_action_just_pressed("scroll_down"):
 		position.y += 100
 
 func _process(delta):
-	
+
 	if Input.is_action_pressed("ui_up"):
 		position.y -= speed * delta / 2
-	
+
 	if Input.is_action_pressed("ui_down"):
 		position.y += speed * delta / 2
-	
+
 	if Input.is_action_pressed("ui_left"):
 		position.x -= speed * delta / 2
-	
+
 	if Input.is_action_pressed("ui_right"):
 		position.x += speed * delta / 2
