01858251a846188de2cb46b2a37f5e6059c029fb
Aaron Winter <winter.aaron93@gmail.com>
2018-10-10 16:50:00 +0200

    more presice movement for units

--- game/MoveHandler.gd
+++ game/MoveHandler.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var speed = 400
+var speed = 350.0
 
 var unit
 var path = []
@@ -8,41 +8,57 @@ var path = []
 var init_path_size = 0
 var last_point
 
+var velocity = Vector2(0, 0)
+
 onready var game = $".."
 onready var terrain = $"../Terrain"
 
 func _process(delta):
 	
-	if path:
+	if path.size() > 0:
+		var terrain_type = game.get_terrain_type_at_cell(path[0])
+		if unit.current_moves - unit.movement[terrain_type] < 0:
+			unit.position = terrain.world_to_world_centered(unit.position)
+			velocity = Vector2(0, 0)
+			unit = null
+			init_path_size = 0;
+			path = []
+			# print("path clear")
+	
+	if path.size() > 0 and unit:
+		
+		var terrain_type = game.get_terrain_type_at_cell(path[0])
 		
 		if init_path_size == 0:
 			init_path_size = path.size()
 		
-		var terrain_type = game.get_terrain_type_at_cell(path[0])
+		unit.position += velocity * delta
 		
-		var direction = get_move_direction()
-		var velocity = direction * speed * delta
-		
-		unit.position += velocity
-		
-		if unit.position * direction > terrain.map_to_world_centered(path[0])  * direction:
+		if unit.position.distance_to(terrain.map_to_world_centered(path[0])) <= 6:
 			
 			if path.size() < init_path_size:
-				unit.current_moves -= 1
+				unit.current_moves -= unit.movement[terrain_type]
 			
 			last_point = path[0]
 			path.remove(0)
 			
-			if !path or unit.current_moves == 0:
+			if path.size() == 0 or unit.current_moves == 0:
 				
-				if unit.current_moves == 0:
-					path.clear()
+				if unit.current_moves <= 0:
+					unit.current_moves = 0
+					path = []
 					game.active_unit = null
 					game.active_unit_path = []
 				
 				unit.position = terrain.world_to_world_centered(unit.position)
+				velocity = Vector2(0, 0)
 				unit = null
 				init_path_size = 0;
+			else:
+				var target_position = terrain.map_to_world_centered(path[0])
+				var angle = unit.get_angle_to(target_position)
+				velocity = Vector2(speed*cos(angle), speed*sin(angle))
+				# print(velocity)
 
 func move_unit(unit, path):
 	
@@ -51,8 +67,3 @@ func move_unit(unit, path):
 	if path:
 		last_point = terrain.map_to_world_centered(unit.position)
 		self.unit = unit
-
-
-func get_move_direction():
-	return (terrain.map_to_world_centered(path[0]) - terrain.map_to_world_centered(last_point)).normalized()
-	
