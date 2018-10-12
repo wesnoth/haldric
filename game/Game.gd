d5f37a6d9961a68cc108c8611e24dce84666bd95
zozer firehood <firehood.co@gmail.com>
2018-10-12 16:17:09 -0400

    trying to fix things

--- game/Game.gd
+++ game/Game.gd
@@ -44,7 +44,6 @@ func _input(event):
 
 		if (terrain.check_boundaries(mouse_cell)):
 			print("Village: ", terrain.tiles[terrain.flatten_v(mouse_cell)].is_village)
-			print(terrain.tiles[terrain.flatten_v(mouse_cell)].terrain_type)
 		if is_unit_at_cell(mouse_cell) and active_unit == null:
 			active_unit = get_unit_at_cell(mouse_cell)
 
--- game/MoveHandler.gd
+++ game/MoveHandler.gd
@@ -11,14 +11,13 @@ var last_point
 var velocity = Vector2(0, 0)
 
 onready var game = $".."
-onready var terrain = $"../Terrain"
 
 func _process(delta):
 
 	if path.size() > 1:
 		var terrain_type = game.get_terrain_type_at_cell(path[1])
 		if unit.current_moves - unit.get_movement_cost(terrain_type) < 0:
-			unit.position = terrain.world_to_world_centered(unit.position)
+			unit.position = game.terrain.world_to_world_centered(unit.position)
 			velocity = Vector2(0, 0)
 			unit = null
 			init_path_size = 0;
@@ -34,7 +33,7 @@ func _process(delta):
 
 		unit.position += velocity * delta
 
-		if unit.position.distance_to(terrain.map_to_world_centered(path[0])) <= 6:
+		if unit.position.distance_to(game.terrain.map_to_world_centered(path[0])) <= 6:
 
 			if path.size() < init_path_size:
 				unit.current_moves -= unit.get_movement_cost(terrain_type)
@@ -50,12 +49,12 @@ func _process(delta):
 					game.active_unit = null
 					game.active_unit_path = []
 
-				unit.position = terrain.world_to_world_centered(unit.position)
+				unit.position = game.terrain.world_to_world_centered(unit.position)
 				velocity = Vector2(0, 0)
 				unit = null
 				init_path_size = 0;
 			else:
-				var target_position = terrain.map_to_world_centered(path[0])
+				var target_position = game.terrain.map_to_world_centered(path[0])
 				var angle = unit.get_angle_to(target_position)
 				velocity = Vector2(speed*cos(angle), speed*sin(angle))
 				# print(velocity)
@@ -65,5 +64,5 @@ func move_unit(unit, path):
 	self.path = path
 
 	if path:
-		last_point = terrain.map_to_world_centered(unit.position)
+		last_point = game.terrain.map_to_world_centered(unit.position)
 		self.unit = unit
