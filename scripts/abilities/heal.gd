e9d39d070b5367b8d8fc840a7253e91939d62caf
Aaron Winter <winter.aaron93@gmail.com>
2018-10-25 15:33:28 +0200

    added heal ability for shaman

---
+++ scripts/abilities/heal.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+func heal(unit, params):
+	var adjacent_units = unit.get_adjacent_units()
+	for u in adjacent_units:
+		if unit.side == u.side:
+			u.heal(params.value)
+			print(unit.id, " healed ", u.id, " by ", params.value, " HP")
