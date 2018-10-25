2552afa4e1a7568f9fb601f151b421b60a6af657
Aaron Winter <winter.aaron93@gmail.com>
2018-10-25 20:11:03 +0200

    follow up commit on the restructuring

--- global/Wesnoth.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-signal unit_moved(event, unit)
-signal unit_move_finished(unit)
-
-signal turn_refresh(event, side)
-signal turn_end(event, side)
--- interface/popup/AttackPopup.gd
+++
@@ -1,9 +0,0 @@
-extends PopupMenu
-
-func add_attacks(attacks):
-	clear()
-
-	var index = 0
-	for attack in attacks:
-		add_icon_item(load(attack.icon), str(attack.name, ": ", attack.damage, " x ", attack.strikes, " (", attack.type, ")"), index)
-		index += 1
