8ae164bfa85f32029c5dd1acd1eae44d5c8c2766
Aaron Winter <winter.aaron93@gmail.com>
2018-11-06 13:41:13 +0100

    added level plate

--- source/interface/components/UnitInfo.gd
+++ source/interface/components/UnitInfo.gd
@@ -1,11 +1,13 @@
 extends Control
 
+onready var level = $"LevelPlate/Label"
 onready var unit_name = $"Info/Name"
 onready var type = $"Info/Type"
 onready var alignment = $"Info/Alignment"
 onready var race = $"Info/Race"
 
 func update_unit_info(unit):
+	level.text = str(unit.level)
 	unit_name.set_text(str(unit.name))
 	type.set_text(str(unit.type))
 	alignment.set_text(str(unit.alignment))
@@ -14,6 +16,7 @@ func update_unit_info(unit):
 
 func clear_unit_info():
 	hide()
+	level.text = "-"
 	unit_name.set_text("-")
 	type.set_text("-")
 	alignment.set_text("-")
--- source/interface/components/UnitStats.gd
+++ source/interface/components/UnitStats.gd
@@ -1,15 +1,10 @@
 extends Control
 
-onready var level = $"Stats/Level"
 onready var hp = $"Stats/HP"
 onready var xp = $"Stats/XP"
 onready var mp = $"Stats/MP"
 
-func ready():
-	level.set_value(" ")
-
 func update_unit_stats(unit):
-	level.set_stat(unit.level)
 	hp.set_value(str(unit.current_health, "/", unit.base_max_health))
 	xp.set_value(str(unit.current_experience, "/", unit.base_experience))
 	mp.set_value(str(unit.current_moves, "/", unit.base_max_moves, " | ", unit.current_defense, "%"))
