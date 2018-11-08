5e54be72c5abd6b9897fc478072f782b5de2c2b8
Aaron Winter <winter.aaron93@gmail.com>
2018-11-08 13:27:07 +0100

    added config files for decals + decal animation support

--- LB/LBTileMap.gd
+++ LB/LBTileMap.gd
@@ -1,58 +1,81 @@
 extends TileMap
 
 export (int) var random_seed = 1
-export (Vector2) var scale_range = Vector2(0.8, 1.2)
-export (int, 0, 100) var offset_range = 60
-export (float, 0, 2.99) var decals = 0.5
 export (float, 0, 0.99) var flip = 0.5
 
 var SHADER = preload("res://LB/tile.shader")
 var MASK = preload("res://LB/mask.png")
 
-var DETAILS = [
-	preload("res://LB/Test_grass_decal1.png"),
-	preload("res://LB/Test_grass_decal2.png"),
-	preload("res://LB/Test_grass_decal3.png")
-]
+var decals = {}
 
 var string 
 
 onready var detail_container = $"Details"
 
+
 func _ready():
+	_load_decal_dir("res://LB/decals/config")
+	
 	seed(random_seed)
 	
 	for cell in get_used_cells():
-		var pos = map_to_world(cell)
 		var used = []
-		for i in range(int(decals) + 1):
-			var rand = randf() + i
-			string = str(i + 1, " » ", rand)
-			var decal = randi() % 3
-			if rand < decals and not decal in used:
-				string += str(", decal set")
-				var tex = DETAILS[decal]
+		for decal in decals.values():
+			decal["pos"] = Vector2(0,0)
+			decal.pos = map_to_world(cell)
+			if randf() < decal.probability and !used.has(decal):
+				add_detail(decal)
 				used.append(decal)
-				add_detail(tex, pos)
-			print(string)
-			
-func add_detail(tex, pos):
-	var sprite = Sprite.new()
+
+
+func add_detail(decal):
+	var sprite = null
+	
+	if decal.has("image"):
+		sprite = Sprite.new()
+		sprite.texture = decal.image
+	
+	if decal.has("animation"):
+		sprite = AnimatedSprite.new()
+		sprite.frames = decal.frames
+		sprite.play("default")
+		
 	var offset = Vector2(0, 0)
 	var scale = Vector2(0, 0)
-	
-	offset.x = ((randi() % (offset_range + 1)) - offset_range / 2) * 2 + 36
-	offset.y = ((randi() % (offset_range + 1)) - offset_range / 2) * 2 + 36
-	
-	scale = rand_range(scale_range.y, scale_range.x)
-	
-	pos += offset
-	
+
+	offset.x = decal.offset.x + ((randi() % (decal.placement_range.x + 1)) - decal.placement_range.x / 2) * 2
+	offset.y = decal.offset.z + ((randi() % (decal.placement_range.z + 1)) - decal.placement_range.z / 2) * 2
+
+	scale = rand_range(decal.scale_range.min, decal.scale_range.max)
+
+	decal.pos += offset
+
 	if randf() < flip:
 		sprite.flip_h = true
-	sprite.texture = tex
-	sprite.position = pos
+	sprite.centered = true
+	sprite.position = decal.pos
 	sprite.scale = Vector2(scale, scale)
-	
+	sprite.z_index = decal.layer
+	print(sprite)
 	detail_container.add_child(sprite)
-	string += str(", offset: ", offset, ", scale: ", Vector2(scale, scale))
+
+
+func _load_decal_dir(path):
+	var files = []
+	files = Registry.get_files_in_directory(path, files)
+	for file in files:
+		var config = Registry.yaml_parser.parse(file.data.get_as_text())
+		decals[file.id] = config
+		decals[file.id].id = file.id
+		
+		if config.has("image"):
+			config.image = load(config.image)
+		
+		if config.has("animation"):
+			var frames = SpriteFrames.new()
+			for i in range(config.animation.frames.size()):
+				var tex = load(config.animation.frames[i])
+				frames.add_frame("default", tex, i)
+			frames.set_animation_speed("default", config.animation.fps)
+			frames.set_animation_loop("default", true)
+			decals[file.id].frames = frames
