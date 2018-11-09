04f442c7d65b52776b119391aa8249da2e8c37cf
Aaron Winter <winter.aaron93@gmail.com>
2018-11-09 23:05:03 +0100

    add option to use tilesets instead of individual sprites for decals. using tilesets does not allow random displacement | scaling though

--- LB/LBTileMap.gd
+++ LB/LBTileMap.gd
@@ -1,5 +1,7 @@
 extends TileMap
 
+export (Vector2) var map_size = Vector2(40, 40)
+export (bool) var use_layers = false
 export (int) var random_seed = 1
 export (float, 0, 0.99) var flip = 0.5
 
@@ -12,12 +14,36 @@ var string
 
 onready var detail_container = $"Details"
 
+# this is only to get the camera from Game.gd working without having to change its code
+onready var terrain = self
+
+onready var layers = [
+	$"Layer0",
+	$"Layer1",
+	$"Layer2",
+	$"Layer3",
+	$"Layer4",
+]
 
 func _ready():
+	generate_map()
 	_load_decal_dir("res://LB/decals/json")
+	generate_tile_set()
 	
 	seed(random_seed)
 	
+	if use_layers:
+		use_tile_set()
+	else:
+		use_sprites()
+
+func use_tile_set():
+	for cell in get_used_cells():
+		for decal in decals.values():
+			if randf() < decal.probability:
+				layers[decal.layer].set_cellv(cell + Vector2(1, 1), decal.index)
+
+func use_sprites():
 	for cell in get_used_cells():
 		var used = []
 		for decal in decals.values():
@@ -26,7 +52,6 @@ func _ready():
 			if randf() < decal.probability and !used.has(decal):
 				add_detail(decal)
 				used.append(decal)
-
 
 func add_detail(decal):
 	var sprite = null
@@ -56,9 +81,24 @@ func add_detail(decal):
 	sprite.position = decal.pos
 	sprite.scale = Vector2(scale, scale)
 	sprite.z_index = decal.layer
-	print(sprite)
 	detail_container.add_child(sprite)
 
+func generate_tile_set():
+	var index = 0
+	var tileset = TileSet.new()
+	for decal in decals.values():
+		decal["index"] = index;
+		tileset.create_tile(index)
+		tileset.tile_set_texture(index, decal.image)
+		tileset.tile_set_region(index, Rect2(Vector2(0,0), decal.image.get_size()))
+		index += 1
+	for layer in layers:
+		layer.tile_set = tileset
+
+func generate_map():
+	for y in range(map_size.y):
+		for x in range(map_size.x):
+			set_cell(x, y, 0)
 
 func _load_decal_dir(path):
 	var files = []
