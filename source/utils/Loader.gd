d208c32aac914a17c064b79e6a4854c3cabde871
Aaron Winter <winter.aaron93@gmail.com>
2019-02-17 23:50:30 +0100

    Loader: extract loading a file into a separate function

--- source/utils/Loader.gd
+++ source/utils/Loader.gd
@@ -1,17 +1,22 @@
 class_name Loader extends Node
 
+
+static func load_file(path : String) -> Dictionary:
+	return _get_file_data(path)
+
+
 static func load_dir(path : String) -> Array:
 	return _get_file_data_in_directory(path, [])
 
-static func _get_file_data_in_directory(path : String, data : Array) -> Array:
+
+static func _get_file_data_in_directory(path : String, directory_data : Array) -> Array:
 	
 	var directory := Directory.new()
 	
-	if not directory.dir_exists(path):
+	if not directory.dir_exists(path) or not directory.open(path) == OK:
 		print("Loader: failed to load ", path, ", return []")
 		return []
 	
-	directory.open(path)
 	directory.list_dir_begin(true, true)
 	
 	var sub_path := ""
@@ -26,19 +31,31 @@ static func _get_file_data_in_directory(path : String, data : Array) -> Array:
 			break
 		
 		elif directory.current_is_dir():
-			_get_file_data_in_directory(directory.get_current_dir() + "/" + sub_path, data)
+			_get_file_data_in_directory(directory.get_current_dir() + "/" + sub_path, directory_data)
 		
 		else:
-			var file := File.new()
-			var file_id := sub_path.split(".")[0]
-			
-			if file.open(directory.get_current_dir() + "/" + sub_path, file.READ) == OK:
-				data.append({ text = file.get_as_text(), id = file_id })
-				
-				print("Loader: load file: ", directory.get_current_dir() + "/" + sub_path)
-			
-			file.close()
+			var file_data = _get_file_data(directory.get_current_dir() + "/" + sub_path)
+			directory_data.append(file_data)
 	
 	directory.list_dir_end()
+	return directory_data
 
-	return data
+
+static func _get_file_data(path : String) -> Dictionary:
+	
+	var file := File.new()
+	var file_id := path.split(".")[0]
+	var file_data := {}
+	
+	if file.open(path, file.READ) == OK:
+		file_data = { text = file.get_as_text(), id = file_id }
+		
+		print("Loader: load file: ", path)
+	
+	else:
+		file_data = {}
+		
+		print("Loader: failed to load file: ", path, ", return {}")
+	
+	file.close()
+	return file_data
