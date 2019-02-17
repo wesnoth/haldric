b6ac26f8e763e43ddd6e5e8006175bcf32b797b5
Aaron Winter <winter.aaron93@gmail.com>
2019-02-17 17:12:54 +0100

    fix Map.gd

--- source/map/Map.gd
+++ source/map/Map.gd
@@ -1,16 +1,22 @@
 class_name Map extends TileMap
 
+const OFFSET = Vector2(36, 36)
+
 var width := 0
 var height := 0
 
 var locations := []
 
 func _ready():
+	_initialize_locations()
 	width = get_used_rect().size.x
 	height = get_used_rect().size.y
 
 func map_to_world_centered(cell : Vector2) -> Vector2:
 	return map_to_world(cell) + OFFSET
+
+func world_to_world_centered(cell: Vector2) -> Vector2:
+	return map_to_world_centered(world_to_map(cell))
 
 func _initialize_locations():
 	for y in range(height):
@@ -21,7 +27,7 @@ func _initialize_locations():
 			loc.id = id
 			loc.cell = Vector2(x, y)
 			loc.position = map_to_world(cell)
-			locations[id] = Location.new(self, cell)
+			locations[id] = loc
 
-func _flatten(cell):
+func _flatten(cell : Vector2) -> int:
 	return int(cell.y) * int(width) + int(cell.x)
