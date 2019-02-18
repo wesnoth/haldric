4f897936c45a331b9a5fff593b38597c5e9a77d3
Aaron Winter <winter.aaron93@gmail.com>
2019-02-18 12:11:46 +0100

    static typing fixes to Map and Location

--- source/map/Location.gd
+++ source/map/Location.gd
@@ -2,13 +2,13 @@ class_name Location
 
 const OFFSET = Vector2(36, 36)
 
-var id := ""
+var id := 0
 
 var cell := Vector2()
 var position := Vector2()
 
-var terrain := null
-var unit := null
+var terrain : Terrain = null
+var unit : Unit = null
 
 func get_position_centered() -> Vector2:
 	return position + OFFSET
--- source/map/Map.gd
+++ source/map/Map.gd
@@ -18,12 +18,12 @@ func map_to_world_centered(cell : Vector2) -> Vector2:
 func world_to_world_centered(cell: Vector2) -> Vector2:
 	return map_to_world_centered(world_to_map(cell))
 
-func _initialize_locations():
+func _initialize_locations() -> void:
 	for y in range(height):
 		for x in range (width):
-			var cell = Vector2(x, y)
-			var id = _flatten(cell)
-			var loc = Location.new()
+			var cell := Vector2(x, y)
+			var id := _flatten(cell)
+			var loc := Location.new()
 			loc.id = id
 			loc.cell = Vector2(x, y)
 			loc.position = map_to_world(cell)
