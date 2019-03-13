7774ec124f3fbfde301c21caace518706f31525e
Aaron Winter <winter.aaron93@gmail.com>
2019-03-13 17:04:12 +0100

    move_to function for units

--- source/game/Game.gd
+++ source/game/Game.gd
@@ -2,9 +2,12 @@ extends Node2D
 
 var map : Map
 
+onready var map_container = $MapContainer
+onready var unit_container = $UnitContainer
+
 func _ready():
 	_initialize()
 
 func _initialize():
 	map = Loader.load_map(Registry.scenarios["scenario"].map_data)
-	add_child(map)
+	map_container.add_child(map)
--- source/interface/menus/TitleScreen.gd
+++ source/interface/menus/TitleScreen.gd
@@ -3,6 +3,5 @@ extends Control
 func _on_Singleplayer_pressed() -> void:
 	Scene.change(Scene.Game)
 
-
 func _on_Quit_pressed() -> void:
 	get_tree().quit()
