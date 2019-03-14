2ec70cdd54f02064e3524dea3b118204f0ec11e4
Aaron Winter <winter.aaron93@gmail.com>
2019-03-14 16:38:58 +0100

    added RTerrain plus a few examples in res://data/terrain/ but it is not yet ready to be used

---
+++ source/templates/RTerrain.gd
@@ -0,0 +1,11 @@
+class_name RTerrain extends Resource
+
+export(String) var code := ""
+export(String) var name := ""
+export(String) var type := ""
+
+export(bool) var recruit_onto := false
+export(bool) var recruit_from := false
+
+export(bool) var heals := false
+export(bool) var gives_income := false
