6e13bdd21787fa224a0717bd9c254bab6c7e5828
Aaron Winter <winter.aaron93@gmail.com>
2019-03-16 14:32:17 +0100

    added version

--- source/global/Global.gd
+++ source/global/Global.gd
@@ -3,8 +3,17 @@ extends Node
 # Resources
 var tileset = preload("res://graphics/tilesets/tileset.tres")
 
-var fullscreen = false
+var version = _get_version_string()
+
 # Toggle Fullscreen
 func _input(event):
 	if event.is_action_pressed("toggle_fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen
+
+func _get_version_string():
+	var version = ""
+	version += str(ProjectSettings.get("application/version/major"), ".")
+	version += str(ProjectSettings.get("application/version/minor"), ".")
+	version += str(ProjectSettings.get("application/version/patch"), "-")
+	version += str(ProjectSettings.get("application/version/status"))
+	return version
--- source/interface/menus/TitleScreen.gd
+++ source/interface/menus/TitleScreen.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _ready():
 	Audio.play(Registry.music.return_to_wesnoth)
+	$Version.text = Global.version
 
 func _on_Singleplayer_pressed() -> void:
 	Scene.change(Scene.Game)
