23dd72fd1118ccf882fcde663cc8ffa55b89bb08
Charles Dang <exodia339@gmail.com>
2019-03-18 01:11:21 +1100

    Implement loading scenarios as scenes

--- source/core/game/Game.gd
+++ source/core/game/Game.gd
@@ -7,16 +7,16 @@ onready var unit_container = $UnitContainer
 
 func _ready():
 	_load_map()
-	_load_units()
+#	_load_units()
 
 func _load_map() -> void:
-	map = Loader.load_map(Registry.scenarios["scenario"].map_data)
-	map_container.add_child(map)
+	var scenario = load(Registry.scenarios["tilemap_test"]).instance()
+	map_container.add_child(scenario)
 
-func _load_units() -> void:
-	var unit = Unit.new(Registry.units["Archer"])
-	add_child(unit)
-	unit.move_to(map.get_location(Vector2(4, 4)))
+#func _load_units() -> void:
+#	var unit = Unit.new(Registry.units["Archer"])
+#	add_child(unit)
+#	unit.move_to(map.get_location(Vector2(4, 4)))
 
 func _on_SaveMap_pressed() -> void:
 	Loader.save_map(map)
