402ca44c4993386751ae31ced0c6a23cc53e3bc2
Aaron Winter <winter.aaron93@gmail.com>
2019-03-18 09:26:33 +0100

    Side.gd static typing cleanup

--- source/core/scenario/Side.gd
+++ source/core/scenario/Side.gd
@@ -1,57 +1,56 @@
 class_name Side extends Node2D
 
+const income_per_village := 2
 
-const income_per_village : int = 2
+const heal_on_village := 8
+const heal_on_rest := 2
 
-const heal_on_village : int = 8
-const heal_on_rest : int = 2
+var base_income := 2
 
-var base_income : int = 2
+var team_color := ""
+var team_color_info := []
+var side := 0
 
-var team_color : String
-var team_color_info : Array
-var side : int = 0
-
-var gold : int = 100
-var income : int = 2
+var gold := 100
+var income := 2
 
 var shader : ShaderMaterial = null
 var flag_shader : ShaderMaterial = null
 
-var recruit : Array = []
+var recruit := []
 
-var villages : Array = []
+var villages := []
 
-var leaders : Array = []
+var leaders := []
 
-var units : Array = []
+var units := []
 
-func initialize(side, gold = 100, base_income = 2, starting_villages = []):
+func initialize(side : int, gold := 100, base_income := 2, starting_villages := []) -> void:
 	self.base_income = base_income
 	self.side = side
 	self.gold = gold
 	self.villages = starting_villages
 	calculate_income()
 
-func add_village(cell):
+func add_village(cell : Vector2) -> void:
 	villages.append(cell)
 
-func remove_village(cell):
+func remove_village(cell : Vector2) -> void:
 	villages.erase(cell)
 
-func has_village(cell):
+func has_village(cell : Vector2) -> bool:
 	return villages.has(cell)
 
-func get_villages():
+func get_villages() -> Array:
 	return villages
 
-func calculate_income():
+func calculate_income() -> void:
 	income = income_per_village * villages.size() + base_income
 
-func turn_refresh():
+func turn_refresh() -> void:
 	gold += income
 
-func get_first_leader():
+func get_first_leader() -> Unit:
 	if leaders.size() > 0:
 		return leaders[0]
 	return null
