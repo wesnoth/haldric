4895964dcee591c794f2d227b73c7de3d48fccca
Aaron Winter <winter.aaron93@gmail.com>
2019-03-18 18:12:42 +0100

    fix units not showing up

--- source/core/scenario/Scenario.gd
+++ source/core/scenario/Scenario.gd
@@ -11,17 +11,17 @@ func initialize() -> void:
 	TeamColor.initializeFlagColors()
 
 func add_unit(side_number : int, unit_id : String, x : int, y : int) -> void:
-	
-	if side_number >= sides.get_child_count():
+
+	if side_number > sides.get_child_count():
 		return
-	
+
 	var side : Side = sides.get_child(side_number-1)
 	var unit = Wesnoth.Unit.instance()
-	
+
 	side.add_child(unit)
 	unit.initialize(Registry.units[unit_id])
-	
+
 	var loc = map.get_location(Vector2(x, y))
 	unit.place_at(loc)
-	
+
 	unit.sprite.set_material(side.shader)
