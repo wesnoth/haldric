140b53072595236ef3d702778c4418f13fc1c73d
Aaron Winter <winter.aaron93@gmail.com>
2019-03-18 10:16:35 +0100

    added number of sides as export variable to scenario

--- source/core/scenario/Scenario.gd
+++ source/core/scenario/Scenario.gd
@@ -2,6 +2,7 @@ class_name Scenario extends Node2D
 
 export(String) var title = ""
 export(int) var turns = 99
+export(int) var side_number = 2
 
 onready var map = $Map
 onready var units = $Units
@@ -9,7 +10,7 @@ onready var sides = $Sides
 
 func initialize() -> void:
 	TeamColor.initializeFlagColors()
-	for i in range(2):
+	for i in range(sides):
 		var side := Side.new()
 		side.initialize(i)
 		side.team_color = TeamColor.team_color_data.keys()[i]
@@ -18,8 +19,8 @@ func initialize() -> void:
 		side.shader = TeamColor.generate_team_shader(side.team_color_info)
 		sides.add_child(side)
 
-func add_unit(unit : Unit, cell : Vector2, sideNum : int) -> void:
-	var side : Side = sides.get_child(sideNum-1)
+func add_unit(unit : Unit, cell : Vector2, side_number : int) -> void:
+	var side : Side = sides.get_child(side_number-1)
 	side.add_child(unit)
 	var loc = map.get_location(cell)
 	unit.move_to(loc)
