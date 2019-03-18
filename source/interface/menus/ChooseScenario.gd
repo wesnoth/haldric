28fdebdea2c0f12e735d3aee9eaf11dec8bcb42a
Aaron Winter <winter.aaron93@gmail.com>
2019-03-18 18:02:55 +0100

    make the scenario choose scene play the scenario typed in on enter

--- source/interface/menus/ChooseScenario.gd
+++ source/interface/menus/ChooseScenario.gd
@@ -2,6 +2,10 @@ extends Control
 
 onready var line_edit = $CenterContainer/VBoxContainer/LineEdit
 
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		_on_Play_pressed()
+
 func _on_Back_pressed():
 	Scene.change(Scene.TitleScreen)
 
