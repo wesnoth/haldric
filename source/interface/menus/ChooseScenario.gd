c01547737d2ac20f07f8c57e655aaa3f96a139a2
Aaron Winter <winter.aaron93@gmail.com>
2019-03-18 19:49:35 +0100

    select scenario via option button

--- source/interface/menus/ChooseScenario.gd
+++ source/interface/menus/ChooseScenario.gd
@@ -1,16 +1,23 @@
 extends Control
 
-onready var line_edit = $CenterContainer/VBoxContainer/LineEdit
+onready var option_button = $CenterContainer/VBoxContainer/OptionButton
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		_on_Play_pressed()
 
+func _ready():
+	var i = 0
+	for scenario in Registry.scenarios.keys():
+		option_button.add_item(scenario, i)
+		option_button.set_item_metadata(i, scenario)
+		i += 1
+
 func _on_Back_pressed():
 	Scene.change(Scene.TitleScreen)
 
 func _on_Play_pressed():
-	if not Registry.scenarios.has(line_edit.text):
-		return
-	Global.scenario_name = line_edit.text
+	var scenario = option_button.get_selected_metadata()
+
+	Global.scenario_name = scenario
 	Scene.change(Scene.Game)
