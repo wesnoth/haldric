95ed81e9a9adf0d9a82c55406bb25ecdec3e8ed2
Aaron Winter <winter.aaron93@gmail.com>
2019-03-18 14:07:23 +0100

    make units movable: select with left mouse click, move with left mouse click, deselect with right mouse click

--- source/core/game/Game.gd
+++ source/core/game/Game.gd
@@ -4,6 +4,22 @@ var scenario : Scenario = null
 
 onready var scenario_container = $ScenarioContainer
 
+var selected_unit = null
+
+func _unhandled_input(event : InputEvent) -> void:
+	
+	if event.is_action_pressed("mouse_left"):
+		var mouse_cell = scenario.map.world_to_map(get_global_mouse_position())
+		print(mouse_cell)
+		var location = scenario.map.get_location(mouse_cell)
+		if location.movable:
+			selected_unit = location.movable
+		elif selected_unit and not location.movable:
+			selected_unit.move_to(location)
+	
+	elif event.is_action_pressed("mouse_right"):
+		selected_unit = null
+
 func _ready():
 	_load_map()
 	_load_units()
@@ -17,3 +33,4 @@ func _load_map() -> void:
 func _load_units() -> void:
 	if scenario:
 		scenario.add_unit(2, "Archer", 4, 4)
+		scenario.add_unit(2, "Archer", 7, 8)
