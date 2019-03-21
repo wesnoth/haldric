8fe8556596af4829c7b09b0d3c06d55704c669ef
Charles Dang <exodia339@gmail.com>
2019-03-22 06:48:10 +1100

    Small code cleanup

--- source/global/Registry.gd
+++ source/global/Registry.gd
@@ -15,26 +15,18 @@ func scan() -> void:
 	_load_terrain()
 
 func _load_units() -> void:
-	var directory_data: Array =\
-			Loader.load_dir("res://data/units", ["tres", "res"])
-	for file_data in directory_data:
+	for file_data in Loader.load_dir("res://data/units", ["tres", "res"]):
 		units[file_data.id] = file_data.data
 
 func _load_music() -> void:
-	var directory_data: Array =\
-			Loader.load_dir("res://audio/music", ["ogg", "wav"])
-	for file_data in directory_data:
+	for file_data in Loader.load_dir("res://audio/music", ["ogg", "wav"]):
 		music[file_data.id] = file_data.data
 
 func _load_scenarios() -> void:
-	var directory_data: Array =\
-			Loader.load_dir("res://data/scenarios", ["tres", "res"])
-	for file_data in directory_data:
+	for file_data in Loader.load_dir("res://data/scenarios", ["tres", "res"]):
 		scenarios[file_data.id] = file_data # Save all file data
 
 func _load_terrain() -> void:
-	var directory_data: Array =\
-			Loader.load_dir("res://data/terrain", ["tres", "res"])
-	for file_data in directory_data:
+	for file_data in Loader.load_dir("res://data/terrain", ["tres", "res"]):
 		var code = file_data.data.code
 		terrain[code] = file_data.data
