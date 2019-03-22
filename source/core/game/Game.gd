4d72dab86be7de1c20fd841b775b2a6413f38116
Aaron Winter <winter.aaron93@gmail.com>
2019-03-22 18:00:50 +0100

    comment cleanup

--- source/core/game/Game.gd
+++ source/core/game/Game.gd
@@ -15,24 +15,13 @@ func _unhandled_input(event: InputEvent) -> void:
 		if location:
 			if location.unit:
 				_set_selected_unit(location.unit)
-#				if selected_unit:
-#					selected_unit.unhighlight_moves()
-#				selected_unit = location.unit
-#				selected_unit.reachable = scenario.map.find_all_reachable_cells(selected_unit)
-#				selected_unit.highlight_moves()
 			elif selected_unit and not location.unit:
 				selected_unit.move_to(location)
 				_set_selected_unit(null)
-#				_clear_temp_path()
-#				selected_unit.unhighlight_moves()
-#				selected_unit = null
 
 	elif event.is_action_pressed("mouse_right"):
 		_set_selected_unit(null)
-#		if selected_unit:
-#			selected_unit.unhighlight_moves()
-#			selected_unit = null
-#		_clear_temp_path()
+
 
 	elif event is InputEventMouseMotion:
 		var mouse_cell: Vector2 = scenario.map.world_to_map(get_global_mouse_position())
