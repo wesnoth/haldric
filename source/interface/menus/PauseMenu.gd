a1b668c983440655d8501cf88d6a341c904dd175
Aaron Winter <winter.aaron93@gmail.com>
2019-03-24 17:44:31 +0100

    added pause menu, removed back button

---
+++ source/interface/menus/PauseMenu.gd
@@ -0,0 +1,16 @@
+extends Popup
+
+onready var tween = $Tween
+
+func _input(event : InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		if visible:
+			hide()
+		else:
+			popup()
+
+func _on_Resume_pressed() -> void:
+	hide()
+
+func _on_Quit_pressed() -> void:
+	Scene.change(Scene.TitleScreen)
