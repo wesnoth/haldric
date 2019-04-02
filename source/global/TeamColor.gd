23732d998cc02f76d2474ebbadba92840ceaf681
Charles Dang <exodia339@gmail.com>
2019-04-02 23:47:59 +1100

    Rename shader folder

--- source/global/TeamColor.gd
+++ source/global/TeamColor.gd
@@ -1,7 +1,7 @@
 extends Node
 
-const SHADER = preload("res://graphics/shader/TeamColors.shader")
-const FLAG_SHADER = preload("res://graphics/shader/TeamFlag.shader")
+const SHADER = preload("res://graphics/shaders/TeamColors.shader")
+const FLAG_SHADER = preload("res://graphics/shaders/TeamFlag.shader")
 
 var base_color_map := [
 	Color("F49AC1"),
