23a9ffe23152a60ba8d7b83fa4cd2b5aa8a778a5
Aaron Winter <winter.aaron93@gmail.com>
2019-04-04 12:26:17 +0200

    attempt to fix issues with resizing the window in the titlescreen - not fully working yet

--- source/interface/menus/TitleScreen.gd
+++ source/interface/menus/TitleScreen.gd
@@ -1,26 +1,35 @@
 extends Control
 
-onready var anim = $AnimationPlayer
+onready var tween := $Tween
+onready var anim := $AnimationPlayer as AnimationPlayer
+onready var campaigns := $ChooseCampaign as Control
+onready var lobby := $Lobby as Control
+onready var camera := $Camera2D as Camera2D
 
 func _ready() -> void:
+	get_tree().connect("screen_resized", self, "_on_screen_resized")
 	Audio.play(Registry.music.return_to_wesnoth)
 	$Version.text = Global.version_string
+	_on_screen_resized()
 
 func _on_Singleplayer_pressed() -> void:
 	Scene.change(Scene.ChooseScenario)
 
 func _on_Campaigns_pressed() -> void:
-	anim.play("campaigns_enter")
+	tween.interpolate_property(camera, "position", camera.position, campaigns.rect_position, 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
 
 func _on_ChooseCampaign_back() -> void:
-	anim.play("campaigns_back")
+	tween.interpolate_property(camera, "position", camera.position, Vector2(0, 0), 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
 
 func _on_Lobby_pressed() -> void:
-	# Scene.change(Scene.Lobby)
-	anim.play("lobby_enter")
+	tween.interpolate_property(camera, "position", camera.position, lobby.rect_position, 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
 
 func _on_Lobby_back() -> void:
-	anim.play("lobby_back")
+	tween.interpolate_property(camera, "position", camera.position, Vector2(0, 0), 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
 
 func _on_Editor_pressed() -> void:
 	Scene.change(Scene.Editor)
@@ -31,3 +40,9 @@ func _on_Options_pressed() -> void:
 func _on_Quit_pressed() -> void:
 	get_tree().quit()
 
+func _on_screen_resized() -> void:
+	var size : Vector2 = get_viewport().size
+	campaigns.rect_position.x = size.x
+#	campaigns.rect_size = size
+	lobby.rect_position.x = -size.x
+#	lobby.rect_size = size
