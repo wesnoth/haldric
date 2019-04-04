396cd9b7bc43b97866844d94ffad97da1fb98fe3
Aaron Winter <winter.aaron93@gmail.com>
2019-04-04 21:47:25 +0200

    clearer naming in SidePanel

--- source/interface/hud/SidePanel.gd
+++ source/interface/hud/SidePanel.gd
@@ -1,28 +1,28 @@
 extends Control
 
-onready var flag = $HBoxContainer/Flag as SidePanelItem
-onready var gold = $HBoxContainer/Gold as SidePanelItem
-onready var villages = $HBoxContainer/Villages as SidePanelItem
-onready var units = $HBoxContainer/Units as SidePanelItem
-onready var upkeep = $HBoxContainer/Upkeep as SidePanelItem
-onready var income = $HBoxContainer/Income as SidePanelItem
-onready var battery = $HBoxContainer/Battery as SidePanelItem
-onready var time = $HBoxContainer/Time as SidePanelItem
+onready var flag_item = $HBoxContainer/Flag as SidePanelItem
+onready var gold_item = $HBoxContainer/Gold as SidePanelItem
+onready var villages_item = $HBoxContainer/Villages as SidePanelItem
+onready var units_item = $HBoxContainer/Units as SidePanelItem
+onready var upkeep_item = $HBoxContainer/Upkeep as SidePanelItem
+onready var income_item = $HBoxContainer/Income as SidePanelItem
+onready var battery_item = $HBoxContainer/Battery as SidePanelItem
+onready var time_item = $HBoxContainer/Time as SidePanelItem
 
 func _process(delta):
-	time.set_text(_get_time_string())
-	battery.set_text(str(OS.get_power_percent_left(), " %"))
+	time_item.set_text(_get_time_string())
+	battery_item.set_text(str(OS.get_power_percent_left(), " %"))
 	if OS.get_power_percent_left() == -1:
-		battery.hide()
+		battery_item.hide()
 
 func update_side(scenario : Scenario, side : Side) -> void:
-	flag.set_text(str(side.side))
-	flag.texture_rect.material = side.flag_shader
-	gold.set_text(str(side.gold))
-	units.set_text(str(side.units.get_child_count()))
-	upkeep.set_text(str(side.upkeep))
-	villages.set_text(str(side.villages.size(), "/", scenario.get_village_count()))
-	income.set_text(str(side.income))
+	flag_item.set_text(str(side.side))
+	flag_item.texture_rect.material = side.flag_shader
+	gold_item.set_text(str(side.gold))
+	units_item.set_text(str(side.units.get_child_count()))
+	upkeep_item.set_text(str(side.upkeep))
+	villages_item.set_text(str(side.villages.size(), "/", scenario.get_village_count()))
+	income_item.set_text(str(side.income))
 
 
 func _get_time_string() -> String:
