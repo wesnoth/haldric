9affe5830e857778bfa13a73385630d24dd96349
Aaron Winter <winter.aaron93@gmail.com>
2019-04-04 22:03:58 +0200

    utilized string format in SidePanel

--- source/interface/hud/SidePanel.gd
+++ source/interface/hud/SidePanel.gd
@@ -11,17 +11,17 @@ onready var time_item = $HBoxContainer/Time as SidePanelItem
 
 func _process(delta):
 	time_item.set_text(_get_time_string())
-	battery_item.set_text(str(OS.get_power_percent_left(), " %"))
+	battery_item.set_text( "%d%s" % [OS.get_power_percent_left(), "%"])
 	if OS.get_power_percent_left() == -1:
 		battery_item.hide()
 
 func update_side(scenario : Scenario, side : Side) -> void:
-	flag_item.set_text(str(side.side))
 	flag_item.texture_rect.material = side.flag_shader
+	flag_item.set_text(str(side.side))
 	gold_item.set_text(str(side.gold))
 	units_item.set_text(str(side.units.get_child_count()))
 	upkeep_item.set_text(str(side.upkeep))
-	villages_item.set_text(str(side.villages.size(), "/", scenario.get_village_count()))
+	villages_item.set_text( "%d/%d" % [side.villages.size(), scenario.get_village_count()] )
 	income_item.set_text(str(side.income))
 
 
@@ -29,4 +29,4 @@ func _get_time_string() -> String:
 	var time = OS.get_time()
 	var hour = time.hour
 	var minute = time.minute
-	return str(hour).pad_zeros(2) + " : " + str(minute).pad_zeros(2)
+	return "%s : %s" % [str(hour).pad_zeros(2), str(minute).pad_zeros(2)]
