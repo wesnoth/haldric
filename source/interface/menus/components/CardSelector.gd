1886ff2abe017774d5da4f20c6feadc5caa701c7
Aaron Winter <winter.aaron93@gmail.com>
2019-04-06 20:19:30 +0200

    made cards animate again, also made selectors inherited scenes

--- source/interface/menus/components/CardSelector.gd
+++ source/interface/menus/components/CardSelector.gd
@@ -2,14 +2,13 @@ extends ScrollContainer
 class_name CardSelector
 
 onready var grid_container = $CenterContainer/GridContainer
-onready var cards = grid_container.get_children()
 
 func animate():
 	_hide_all_cards()
-	for card in cards:
-		yield(get_tree().create_timer(1), "timeout")
+	for card in grid_container.get_children():
+		yield(get_tree().create_timer(0.4), "timeout")
 		card.animate()
 
 func _hide_all_cards() -> void:
-	for card in cards:
+	for card in grid_container.get_children():
 		card.modulate = Color("00FFFFFF")
