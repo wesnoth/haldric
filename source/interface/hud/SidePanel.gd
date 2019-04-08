f95173e0961c35b0c2ff6178f074f32283b87463
Charles Dang <exodia339@gmail.com>
2019-04-09 06:22:23 +1100

    Used escape sequence to insert "%" literals

--- source/interface/hud/SidePanel.gd
+++ source/interface/hud/SidePanel.gd
@@ -11,7 +11,7 @@ onready var time_item = $HBoxContainer/Time as SidePanelItem
 
 func _process(delta):
 	time_item.set_text(_get_time_string())
-	battery_item.set_text( "%d%s" % [OS.get_power_percent_left(), "%"])
+	battery_item.set_text("%d%%" % OS.get_power_percent_left())
 	if OS.get_power_percent_left() == -1:
 		battery_item.hide()
 
