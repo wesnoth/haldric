78aa7e1ea0e6cca0695425f2e4e2c0c6757f1982
Aaron Winter <winter.aaron93@gmail.com>
2019-04-10 12:59:47 +0200

    fixed typo. T_T

--- source/scenario/map/Map.gd
+++ source/scenario/map/Map.gd
@@ -28,7 +28,7 @@ onready var transitions := $Transitions as Transitions
 
 onready var cell_selector := $CellSelector as Node2D
 
-onready var map_boder = $MapBorder
+onready var map_border = $MapBorder
 
 func _ready() -> void:
 	_update_size()
@@ -291,14 +291,14 @@ func _update_size() -> void:
 	width = int(get_used_rect().size.x)
 	height = int(get_used_rect().size.y)
 	if width % 2 == 0:
-		map_boder.rect_size = map_to_world(Vector2(width, height)) + Vector2(18, 36)
+		map_border.rect_size = map_to_world(Vector2(width, height)) + Vector2(18, 36)
 	else:
-		map_boder.rect_size = map_to_world(Vector2(width, height)) + Vector2(18, 0)
+		map_border.rect_size = map_to_world(Vector2(width, height)) + Vector2(18, 0)
 
 func _initialize_border() -> void:
 	var size := Vector2(width, height)
 	print(size)
-	map_boder.rect_size = map_to_world(size) + Vector2(18, 36)
+	map_border.rect_size = map_to_world(size) + Vector2(18, 36)
 
 func _initialize_transitions() -> void:
 		transitions.update_transitions(self)
