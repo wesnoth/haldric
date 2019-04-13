a4bbf9ebca017e712d784a340d32d6f2bab37aa0
Aaron Winter <winter.aaron93@gmail.com>
2019-04-13 19:12:59 +0200

    fixed weird fog of war offset

--- source/interface/draw/FogTexture.gd
+++ source/interface/draw/FogTexture.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+const void_texture_offset = Vector2(108, 108)
 var void_texture = preload("res://graphics/images/terrain/fog-uncover.png")
 
 var units
@@ -19,6 +20,6 @@ func _draw() -> void:
 			if unit.side.number == side:
 				unit.visible = true
 				for viewable in unit.viewable:
-					draw_texture(void_texture, viewable.position)
+					draw_texture(void_texture, viewable.position - void_texture_offset)
 					if viewable.unit:
 						viewable.unit.visible = true
