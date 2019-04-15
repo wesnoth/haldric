5b1f0b26a7da556f231709a72c3307c3628bf6be
Aaron Winter <winter.aaron93@gmail.com>
2019-04-15 10:55:09 +0200

    randomize start of stand animation

--- source/unit/states/Idle.gd
+++ source/unit/states/Idle.gd
@@ -3,6 +3,7 @@ extends State
 func _enter(host: Unit):
 	if host.type.anim.has_animation("stand"):
 		host.type.anim.play("stand")
+		host.type.anim.advance(randf() * host.type.anim.current_animation_length)
 
 func _exit(host):
 	pass
