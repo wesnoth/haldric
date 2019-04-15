b2a3093339d3fdf7864bb817fc0721a2f44dd1e3
Charles Dang <exodia339@gmail.com>
2019-04-15 23:14:33 +1100

    Added a loading progress bar for scene switching

--- source/global/Scene.gd
+++ source/global/Scene.gd
@@ -1,19 +1,33 @@
 extends CanvasLayer
 
+signal scene_loading_done
+
 # Scenes
-var TitleScreen := "res://source/interface/menus/TitleScreen.tscn"
-var ChooseScenario := "res://source/interface/menus/ChooseScenario.tscn"
-var Options := "res://source/interface/menus/Options.tscn"
-var Editor := "res://source/editor/Editor.tscn"
-var Game := "res://source/game/Game.tscn"
-var Lobby := "res://source/lobby/Lobby.tscn"
+const TitleScreen := "res://source/interface/menus/TitleScreen.tscn"
+const ChooseScenario := "res://source/interface/menus/ChooseScenario.tscn"
+const Options := "res://source/interface/menus/Options.tscn"
+const Editor := "res://source/editor/Editor.tscn"
+const Game := "res://source/game/Game.tscn"
+const Lobby := "res://source/lobby/Lobby.tscn"
+
 var next_scene := ""
+var loader: ResourceInteractiveLoader = null
 
 onready var anim := $AnimationPlayer as AnimationPlayer
 onready var fps_label := $FPS as Label
+onready var loading_bar := $LoadProgress as ProgressBar
 
 func _process(delta):
 	fps_label.text = str("%d FPS" % Engine.get_frames_per_second())
+
+	if loader == null:
+		return
+
+	match loader.poll():
+		OK:
+			loading_bar.value = loader.get_stage()
+		ERR_FILE_EOF:
+			emit_signal("scene_loading_done")
 
 func change(scene: String) -> void:
 	next_scene = scene
@@ -21,7 +35,14 @@ func change(scene: String) -> void:
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if anim_name == "fade_out":
-		if not get_tree().change_scene(next_scene) == OK:
+		loader = ResourceLoader.load_interactive(next_scene)
+		loading_bar.max_value = loader.get_stage_count() - 1
+
+		yield(self, "scene_loading_done")
+
+		if not get_tree().change_scene_to(loader.get_resource()) == OK:
 			print("Global: failed to load ", next_scene)
+
 		anim.play("fade_in")
 		next_scene = ""
+		loader = null
