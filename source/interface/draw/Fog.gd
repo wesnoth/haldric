b42e041c8a25588f3aff780199517b2545420cb3
Aaron Winter <winter.aaron93@gmail.com>
2019-04-20 20:43:08 +0200

    always update camera offset for fog

--- source/interface/draw/Fog.gd
+++ source/interface/draw/Fog.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 var texture
 
-func _draw() -> void:
+func _process(delta: float) -> void:
 	if Global.Camera:
 		material.set_shader_param("offset", Global.Camera.global_position / get_viewport_rect().size)
 
+func _draw() -> void:
 	if texture:
 		draw_texture(texture, Vector2(0, 0))
