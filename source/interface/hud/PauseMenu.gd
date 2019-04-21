a6aba559cbc46177cb5cc0122bade88885f4ad98
Aaron Winter <winter.aaron93@gmail.com>
2019-04-21 13:53:31 +0200

    pause menu button

--- source/interface/hud/PauseMenu.gd
+++ source/interface/hud/PauseMenu.gd
@@ -1,14 +1,26 @@
-extends Popup
+extends Control
+
+onready var popup := $Popup as Popup
+onready var button := $PauseButton as Button
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		if visible:
-			hide()
-		else:
-			popup()
+		_pupup()
+
+func _pupup() -> void:
+	if popup.visible:
+		popup.hide()
+		button.show()
+	else:
+		popup.popup()
+		button.hide()
 
 func _on_Resume_pressed() -> void:
-	hide()
+	popup.hide()
+	button.show()
 
 func _on_Quit_pressed() -> void:
 	Scene.change(Scene.TitleScreen)
+
+func _on_PauseButton_pressed() -> void:
+	_pupup()
