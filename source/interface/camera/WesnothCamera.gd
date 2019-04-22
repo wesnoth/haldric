wesnoth/haldric

commit 579a4c291a9204eabcaf4d443a61ff7e3143cc72

minor camera refactor

source/interface/camera/WesnothCamera.gd
@@ -8,33 +8,16 @@ var initial_mouse_position := Vector2()
 export var speed := 2000
 
 func _input(event: InputEvent) -> void:
-	var new_position: Vector2 = position
-
-	if Input.is_action_just_pressed("scroll_up"):
-		zoom.x = clamp(zoom.x - 0.5, 0.5, 4)
-		zoom.y = clamp(zoom.y - 0.5, 0.5, 4)
-
-	if Input.is_action_just_pressed("scroll_down"):
-		zoom.x = clamp(zoom.x + 0.5, 0.5, 4)
-		zoom.y = clamp(zoom.y + 0.5, 0.5, 4)
-
-	set_position(new_position)
-
-	if not event is InputEventMouseMotion:
-		if Input.is_mouse_button_pressed(BUTTON_MIDDLE):
-			initial_camera_position = Vector2() + position
-			initial_mouse_position =\
-					Vector2() + get_viewport().get_mouse_position()
-
-	if Input.is_mouse_button_pressed(BUTTON_MIDDLE):
-		set_position(initial_camera_position +\
-				(get_viewport().get_mouse_position() -
-				initial_mouse_position)*-1*zoom)
+	_handle_mouse_scroll(event)
+	_handle_middle_mouse(event)
 
 func _ready() -> void:
 	Global.Camera = self
 
 func _process(delta: float) -> void:
+	_handle_keyboard_scroll(delta)
+
+func _handle_keyboard_scroll(delta: float) -> void:
 	var speed_adjusted: float = speed * zoom.x / 2
 
 	var new_position: Vector2 = position
@@ -52,6 +35,31 @@ func _process(delta: float) -> void:
 		new_position.x += speed_adjusted * delta / 2
 
 	set_position(new_position)
+
+func _handle_mouse_scroll(event: InputEvent) -> void:
+	var new_position: Vector2 = position
+
+	if Input.is_action_just_pressed("scroll_up"):
+		zoom.x = clamp(zoom.x - 0.5, 0.5, 4)
+		zoom.y = clamp(zoom.y - 0.5, 0.5, 4)
+
+	if Input.is_action_just_pressed("scroll_down"):
+		zoom.x = clamp(zoom.x + 0.5, 0.5, 4)
+		zoom.y = clamp(zoom.y + 0.5, 0.5, 4)
+
+	set_position(new_position)
+
+func _handle_middle_mouse(event: InputEvent) -> void:
+	if not event is InputEventMouseMotion:
+		if Input.is_mouse_button_pressed(BUTTON_MIDDLE):
+			initial_camera_position = Vector2() + position
+			initial_mouse_position =\
+					Vector2() + get_viewport().get_mouse_position()
+
+		if Input.is_mouse_button_pressed(BUTTON_MIDDLE):
+			set_position(initial_camera_position +\
+					(get_viewport().get_mouse_position() -
+					initial_mouse_position)*-1*zoom)
 
 func set_position(new_position: Vector2) -> void:
 	position.x = new_position.x

commit 9738a083fa7c66aaa312b5074334d87d6e3ac748

Cleaned up camera code, fixed small regression from 579a4c291a9204eabcaf4d443a61ff7e3143cc72

source/interface/camera/WesnothCamera.gd
@@ -34,7 +34,7 @@ func _handle_keyboard_scroll(delta: float) -> void:
 	if Input.is_action_pressed("ui_right"):
 		new_position.x += speed_adjusted * delta / 2
 
-	set_position(new_position)
+	position = new_position
 
 func _handle_mouse_scroll(event: InputEvent) -> void:
 	var new_position: Vector2 = position
@@ -47,20 +47,13 @@ func _handle_mouse_scroll(event: InputEvent) -> void:
 		zoom.x = clamp(zoom.x + 0.5, 0.5, 4)
 		zoom.y = clamp(zoom.y + 0.5, 0.5, 4)
 
-	set_position(new_position)
+	position = new_position
 
 func _handle_middle_mouse(event: InputEvent) -> void:
 	if not event is InputEventMouseMotion:
 		if Input.is_mouse_button_pressed(BUTTON_MIDDLE):
-			initial_camera_position = Vector2() + position
-			initial_mouse_position =\
-					Vector2() + get_viewport().get_mouse_position()
+			initial_camera_position = position
+			initial_mouse_position = get_viewport().get_mouse_position()
 
-		if Input.is_mouse_button_pressed(BUTTON_MIDDLE):
-			set_position(initial_camera_position +\
-					(get_viewport().get_mouse_position() -
-					initial_mouse_position)*-1*zoom)
-
-func set_position(new_position: Vector2) -> void:
-	position.x = new_position.x
-	position.y = new_position.y
+	if Input.is_mouse_button_pressed(BUTTON_MIDDLE):
+		position = initial_camera_position + (get_viewport().get_mouse_position() - initial_mouse_position) * -1 * zoom