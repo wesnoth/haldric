3d6b75b30ed01b05e2d8666aa83bf791813d3f6b
Aaron Winter <winter.aaron93@gmail.com>
2019-04-23 18:41:31 +0200

    sresources for factions and eras. Not yet wired in

---
+++ source/templates/REra.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name REra
+
+export var image : Texture = null
+export var id := ""
+export var name := ""
+export(Array, String) var factions := []
---
+++ source/templates/RFaction.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name RFaction
+
+export var image : Texture = null
+export var id := ""
+export var name := ""
+export(Array, String) var leader := []
+export(Array, String) var random := []
+export(Array, String) var recruit := []
