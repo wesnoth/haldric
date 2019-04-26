96e31250d53fd93341cbc76a0561a5e4117e4aea
Aaron Winter <winter.aaron93@gmail.com>
2019-04-26 12:48:28 +0200

    fixed fog being all weird when zooming

--- source/interface/draw/Fog.gd
+++ source/interface/draw/Fog.gd
@@ -2,11 +2,6 @@ extends Node2D
 
 var texture
 
-func _process(delta: float) -> void:
-	if Global.Camera:
-		var offset = Global.Camera.global_position / get_viewport_rect().size
-		material.set_shader_param("offset", offset / Global.Camera.zoom)
-
 func _draw() -> void:
 	if texture:
 		draw_texture(texture, Vector2(0, 0))
