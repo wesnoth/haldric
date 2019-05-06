a896d80e78dd483c8e7afe8641f5fc0c711482ef
Charles Dang <exodia339@gmail.com>
2019-05-06 17:06:16 +1100

    Transitions: minor cleanup

--- source/scenario/Transitions.gd
+++ source/scenario/Transitions.gd
@@ -1,13 +1,13 @@
 extends Node2D
 class_name Transitions
 
-var map = null
+var map: Map = null
 
 const tile_set = preload("res://graphics/tilesets/transitions.tres")
 
 var directions = [ "n", "ne", "se", "s", "sw", "nw" ]
 
-onready var layers = [ $"1", $"2", $"3", $"4", $"5", $"6", ]
+onready var layers = get_children()
 
 # map: Map - cyclic reference
 func initialize(map) -> void:
@@ -35,7 +35,7 @@ func _apply_transition_from_cell(cell : Vector2) -> void:
 	var neighbors := Hex.get_neighbors(cell)
 
 	var layer = 0
-	while layer < 6:
+	while layer < layers.size():
 		var n_cell: Vector2 = neighbors[layer]
 		var n_location: Location = map.get_location(n_cell)
 
@@ -56,8 +56,7 @@ func _apply_transition_from_cell(cell : Vector2) -> void:
 		_set_transition_tile(transition.code + "_" + transition.directions, cell, layer)
 		layer += transition.chain
 
-func _get_chain_tile_data(code: String, cell : Vector2, start_direction : int) -> Dictionary:
-
+func _get_chain_tile_data(code: String, cell: Vector2, start_direction: int) -> Dictionary:
 	var neighbors := Hex.get_neighbors(cell)
 
 	var start_cell : Vector2 = neighbors[start_direction]
@@ -91,6 +90,7 @@ func _get_base_terrain_code_from_cell(cell: Vector2) -> String:
 
 	if location:
 		return location.terrain.get_base_code()
+
 	return ""
 
 func _set_transition_tile(transition: String, cell: Vector2, layer: int) -> void:
@@ -98,10 +98,10 @@ func _set_transition_tile(transition: String, cell: Vector2, layer: int) -> void
 	layers[layer].set_cellv(cell, tile_id)
 
 func _tile_set_has_tile(tile_name: String):
-	var tile_id: int = tile_set.find_tile_by_name(tile_name)
-	return tile_id != -1
+	return tile_set.find_tile_by_name(tile_name) != -1
 
-func _get_direction_string(start_direction : int, current_direction : int) -> String:
+func _get_direction_string(start_direction: int, current_direction: int) -> String:
 	if start_direction == current_direction:
 		return directions[current_direction]
+
 	return "-" + directions[current_direction]
