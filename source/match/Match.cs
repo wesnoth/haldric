9b90d5d9b9b45bfddab610f8fae492b708c05652
Aaron Winter <winter.aaron93@gmail.com>
2019-10-20 22:44:34 +0200

    use ReplaceBy to add Scenario to Match

--- source/match/Match.cs
+++ source/match/Match.cs
@@ -5,13 +5,13 @@ public class Match : Node2D
 {
 
     // Child Nodes
-    private Node2D scenarioContainer;
+    private Node2D scenario;
 
     public override void _Ready()
     {
-        scenarioContainer = GetNode("ScenarioContainer") as Node2D;
-        Scenario scenario = GD.Load<PackedScene>("data/scenarios/Test.tscn").Instance() as Scenario;
-        scenarioContainer.AddChild(scenario);
+        scenario = GetNode("Scenario") as Node2D;
+        Scenario newScenario = GD.Load<PackedScene>("data/scenarios/Test.tscn").Instance() as Scenario;
+        scenario.ReplaceBy(newScenario);
         scenario.Owner = this;
     }
 
