59884f24da1d43370a52d4427ca698933d7fd2b5
iwek7 <miwanczuk7@gmail.com>
2019-11-26 19:21:05 +0100

    Prevent np throw when opposing unit does not have attack type that attacker chooses

--- source/unit/Attack.gd
+++ source/unit/Attack.gd
@@ -11,7 +11,7 @@ export var icon : StreamTexture = null
 onready var specials := get_children()
 
 func execute_before_turn(user, target):
-	if not specials.empty():
+	if specials and not specials.empty():
 		for child in specials:
 			if child.has_method("execute_before_turn_to_enemy"):
 				child.execute_before_turn_to_enemy(target)
@@ -21,7 +21,7 @@ func execute_before_turn(user, target):
 				child.execute_before_turn_on_weapon(self)
 
 func execute_each_turn(user, target):
-	if not specials.empty():
+	if specials and not specials.empty():
 		for child in specials:
 			if child.has_method("execute_each_turn_to_enemy"):
 				child.execute_each_turn_to_enemy(target)
@@ -31,7 +31,7 @@ func execute_each_turn(user, target):
 				child.execute_each_turn_on_weapon(self)
 
 func execute_end_turn(user, target):
-	if not specials.empty():
+	if specials and not specials.empty():
 		for child in specials:
 			if child.has_method("execute_end_turn_to_enemy"):
 				child.execute_end_turn_to_enemy(target)
