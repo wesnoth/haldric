5ae1a3710eea32efce1b54e1175bb98e95088fbe
Jay jayjay <jayjay.jay20162016@gmail.com>
2020-05-31 14:55:47 -0400

    add (user/enemy)(hit/miss) events to weaponspecial

--- source/unit/Attack.gd
+++ source/unit/Attack.gd
@@ -30,6 +30,46 @@ func execute_each_turn(user, target, isAttacking):
 			if child.has_method("execute_each_turn_on_weapon"):
 				child.execute_each_turn_on_weapon(self, isAttacking)
 
+func execute_user_hit(user, target, isAttacking):
+	if specials and not specials.empty():
+		for child in specials:
+			if child.has_method("execute_user_hit_to_enemy"):
+				child.execute_user_hit_to_enemy(target, isAttacking)
+			if child.has_method("execute_user_hit_to_user"):
+				child.execute_user_hit_to_user(user, isAttacking)
+			if child.has_method("execute_user_hit_to_weapon"):
+				child.execute_user_hit_to_weapon(self, isAttacking)
+
+func execute_user_miss(user, target, isAttacking):
+	if specials and not specials.empty():
+		for child in specials:
+			if child.has_method("execute_user_miss_to_enemy"):
+				child.execute_user_miss_to_enemy(target, isAttacking)
+			if child.has_method("execute_user_miss_to_user"):
+				child.execute_user_miss_to_user(user, isAttacking)
+			if child.has_method("execute_user_miss_on_weapon"):
+				child.execute_user_miss_on_weapon(self, isAttacking)
+
+func execute_enemy_hit(user, target, isAttacking):
+	if specials and not specials.empty():
+		for child in specials:
+			if child.has_method("execute_enemy_hit_to_enemy"):
+				child.execute_enemy_hit_to_enemy(target, isAttacking)
+			if child.has_method("execute_enemy_hit_to_user"):
+				child.execute_enemy_hit_to_user(user, isAttacking)
+			if child.has_method("execute_enemy_hit_to_weapon"):
+				child.execute_enemy_hit_to_weapon(self, isAttacking)
+
+func execute_enemy_miss(user, target, isAttacking):
+	if specials and not specials.empty():
+		for child in specials:
+			if child.has_method("execute_enemy_miss_to_enemy"):
+				child.execute_enemy_miss_to_enemy(target, isAttacking)
+			if child.has_method("execute_enemy_miss_to_user"):
+				child.execute_enemy_miss_to_user(user, isAttacking)
+			if child.has_method("execute_enemy_miss_on_weapon"):
+				child.execute_enemy_miss_on_weapon(self, isAttacking)
+
 func execute_end_turn(user, target, isAttacking):
 	if specials and not specials.empty():
 		for child in specials:
--- source/unit/WeaponSpecial.gd
+++ source/unit/WeaponSpecial.gd
@@ -10,6 +10,18 @@ func execute_each_turn_to_enemy(target : Unit, isAttacking : bool):
 func execute_end_turn_to_enemy(target : Unit, isAttacking : bool):
 	pass
 
+func execute_user_hit_to_enemy(target : Unit, isAttacking : bool):
+	pass
+
+func execute_user_miss_to_enemy(target : Unit, isAttacking : bool):
+	pass
+
+func execute_enemy_hit_to_enemy(target : Unit, isAttacking : bool):
+	pass
+
+func execute_enemy_miss_to_enemy(target : Unit, isAttacking : bool):
+	pass
+
 func execute_before_turn_to_user(user : Unit, isAttacking : bool):
 	pass
 
@@ -19,6 +31,18 @@ func execute_each_turn_to_user(user : Unit, isAttacking : bool):
 func execute_end_turn_to_user(user : Unit, isAttacking : bool):
 	pass
 
+func execute_user_hit_to_user(target : Unit, isAttacking : bool):
+	pass
+
+func execute_user_miss_to_user(target : Unit, isAttacking : bool):
+	pass
+
+func execute_enemy_hit_to_user(target : Unit, isAttacking : bool):
+	pass
+
+func execute_enemy_miss_to_user(target : Unit, isAttacking : bool):
+	pass
+
 func execute_before_turn_on_weapon(weapon : Attack, isAttacking : bool):
 	pass
 
@@ -26,4 +50,16 @@ func execute_each_turn_on_weapon(weapon : Attack, isAttacking : bool):
 	pass
 
 func execute_end_turn_on_weapon(weapon : Attack, isAttacking : bool):
+	pass
+
+func execute_user_hit_on_weapon(target : Unit, isAttacking : bool):
+	pass
+
+func execute_user_miss_on_weapon(target : Unit, isAttacking : bool):
+	pass
+
+func execute_enemy_hit_on_weapon(target : Unit, isAttacking : bool):
+	pass
+
+func execute_enemy_miss_on_weapon(target : Unit, isAttacking : bool):
 	pass
