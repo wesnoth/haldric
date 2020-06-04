479d8476038714657c2e16de019d613b773f7b9e
Jay jayjay <jayjay.jay20162016@gmail.com>
2020-06-04 08:38:40 -0400

    remove extra spaces, change Poison ID, make Heal check for ID

--- source/unit/abilities/AbilityHeal.gd
+++ source/unit/abilities/AbilityHeal.gd
@@ -7,5 +7,5 @@ export(Array, String) var cure := []
 func _execute(target: Location) -> void:
 	target.unit.heal(amount, true)
 	for effect in target.unit.get_effects():
-		if cure.has(effect.alias):
+		if cure.has(effect.name):
 			target.unit.effects.remove_child(effect)
