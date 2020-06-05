2652bd3b7e0aad55feb2a17644c1bd243858d7bd
Jay jayjay <jayjay.jay20162016@gmail.com>
2020-06-05 17:05:21 -0400

    revert a few titlescreen changes

--- source/menu/TitleScreen.gd
+++ source/menu/TitleScreen.gd
@@ -1,13 +1,10 @@
 extends Control
 
-
-func _on_Play_pressed() -> void:
-	Scene.change("ScenarioSelectionMenu")
-
+func _on_Scenarios_pressed() -> void:
+	Scene.change("Scenarios")
 
 func _on_Editor_pressed() -> void:
-	Scene.change("MapEditor")
-
+	Scene.change("Editor", true, true)
 
 func _on_Quit_pressed() -> void:
 	get_tree().quit()
