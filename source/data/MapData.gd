436e523fc9278d06fdf788e3c7dbd157e5864c8b
Aaron Winter <winter.aaron93@gmail.com>
2020-06-05 20:47:15 +0200

    added width and height to map data

--- source/data/MapData.gd
+++ source/data/MapData.gd
@@ -1,9 +1,15 @@
 extends Resource
 class_name MapData
 
+export var width := 0
+export var height := 0
+
 export var data : Dictionary = {}
 
-func create(width: int, height: int, code: Array) -> void:
+func create(_width: int, _height: int, code: Array) -> void:
+	width = _width
+	height = _height
+
 	for y in height:
 		for x in width:
 			data[Vector2(x, y)] = code
