0034cdb17988b9c2c4532c54944076f93d1fea0a
Aaron Winter <winter.aaron93@gmail.com>
2020-06-07 16:40:37 +0200

    remove theme again as it is not as readable as the default thme

--- source/global/Global.gd
+++ source/global/Global.gd
@@ -2,11 +2,13 @@ extends Node
 
 var selected_scenario : ScenarioData = null
 
+
 func _ready() -> void:
 	Scene.register_scene("TitleScreen", "res://source/menu/TitleScreen.tscn")
 	Scene.register_scene("ScenarioSelectionMenu", "res://source/menu/ScenarioSelectionMenu.tscn")
 	Scene.register_scene("Game", "res://source/game/Game.tscn")
 	Scene.register_scene("MapEditor", "res://source/editor/MapEditor.tscn")
+
 
 func _input(event: InputEvent) -> void:
 	# Toggle Fullscreen
