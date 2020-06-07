6b6f9ea622d8e2f71a058d206d9a1e74317a9642
Charles Dang <exodia339@gmail.com>
2020-06-07 22:30:54 +1100

    Added back fullscreen toggling

--- source/global/Global.gd
+++ source/global/Global.gd
@@ -7,3 +7,9 @@ func _ready() -> void:
 	Scene.register_scene("ScenarioSelectionMenu", "res://source/menu/ScenarioSelectionMenu.tscn")
 	Scene.register_scene("Game", "res://source/game/Game.tscn")
 	Scene.register_scene("MapEditor", "res://source/editor/MapEditor.tscn")
+
+
+func _input(event: InputEvent) -> void:
+	# Toggle Fullscreen
+	if event.is_action_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = not OS.window_fullscreen
