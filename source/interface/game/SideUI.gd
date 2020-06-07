b59625ed0b2b3620f194a884fcfcf729e9ce6bb0
Aaron Winter <winter.aaron93@gmail.com>
2020-06-07 13:16:20 +0200

    more intuitive income numbers

--- source/interface/game/SideUI.gd
+++ source/interface/game/SideUI.gd
@@ -12,6 +12,6 @@ onready var units_label := $HBoxContainer/Units
 func update_info(side: Side) -> void:
 	side_label.text = "Side: %d" % (side.number + 1)
 	gold_label.text = "Gold: %d" % side.gold
-	income_label.text = "Income: %d (-%d)" % [ side.income, side.upkeep ]
+	income_label.text = "Income: %d (%d - %d)" % [ side.income - side.upkeep, side.income, side.upkeep ]
 	villages_label.text = "Villages: %d" % side.villages.size()
 	units_label.text = "Units: %d" % side.units.size()
