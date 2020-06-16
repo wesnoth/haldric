d88aa90fe60390139eefdf1400b045630e63f271
Aaron Winter <winter.aaron93@gmail.com>
2020-06-16 21:30:41 +0200

    added frozen terrain type to units

--- source/unit/unit_type/DefenseType.gd
+++ source/unit/unit_type/DefenseType.gd
@@ -7,5 +7,6 @@ export(int, -100, 100, 5) var hills := 0
 export(int, -100, 100, 5) var water := 0
 export(int, -100, 100, 5) var mountains := 0
 export(int, -100, 100, 5) var sand := 0
+export(int, -100, 100, 5) var frozen := 0
 export(int, -100, 100, 5) var village := 0
 export(int, -100, 100, 5) var castle := 0
--- source/unit/unit_type/MovementType.gd
+++ source/unit/unit_type/MovementType.gd
@@ -7,5 +7,6 @@ export var hills := 2
 export var water := 3
 export var mountains := 3
 export var sand := 3
+export var frozen := 3
 export var village := 1
 export var castle := 1
