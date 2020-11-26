2837732c7cc9146dd8a88c7f83fe14331811c765
Aaron Winter <winter.aaron93@gmail.com>
2020-11-26 13:26:36 +0100

    more cleanup

--- source/unit/trait/TraitQuick.gd
+++ source/unit/trait/TraitQuick.gd
@@ -4,6 +4,7 @@ class_name TraitQuick
 export var health_modifier := 0.95
 export var bonus_moves := 1
 
+
 func _execute(unit: Unit) -> void:
 	unit.health.maximum *= health_modifier
 	unit.moves.maximum += bonus_moves
