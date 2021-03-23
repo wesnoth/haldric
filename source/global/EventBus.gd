4f6ddcdb4cb81834e08729ab45d57e75c4734b31
Jay Jayjay <jayjayjay@example.fios-router.home>
2021-03-23 13:53:37 -0400

    events in miniscenario, clear on refresh, only once

--- source/global/EventBus.gd
+++ source/global/EventBus.gd
@@ -7,6 +7,9 @@ var listeners = {}
 func _ready():
 	pass
 
+func clear():
+	listeners = {}
+
 # void add_listener(object event, object, funcname)
 # adds a function reference to the list of listeners for the given named event
 func add_listener(event, object, funcname):
@@ -14,14 +17,13 @@ func add_listener(event, object, funcname):
 	if not listeners.has(event):
 		listeners[event] = []
 	listeners[event].append(callback)
+	return listeners[event].size()-1
 
 # void remove_listener(object event, object, funcname)
 # removes a function reference from the list of listeners for the given named event
-func remove_listener(event, object, funcname):
-	var callback = funcref(object, funcname)
+func remove_listener(event, ind):
 	if listeners.has(event):
-		if listeners[event].find(callback):
-			listeners[event].remove(callback)
+		listeners[event].remove(ind)
 
 # void raise_event(object event, object args)
 # calls each callback in the list of callbacks in listeners for the given named event, passing args to each
