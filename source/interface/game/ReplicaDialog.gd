f4f8f6287a979c09d3e91ddd0c19d668f4b355ac
Yurii Mazurevych <jmazurevych@gmail.com>
2021-03-26 12:52:21 +0200

    Fixed formatting

--- source/interface/game/ReplicaDialog.gd
+++ source/interface/game/ReplicaDialog.gd
@@ -11,9 +11,11 @@ func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		emit_signal("close_replica")
 
+
 func update_info(data: Replica) -> void:
 	character_name.text = data.character_name
 	replica.text = data.replica
+
 
 func clear() -> void:
 	character_name.text = ""
