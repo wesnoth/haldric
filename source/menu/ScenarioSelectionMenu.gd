406675edf26a84f87f38efce5c4a4f50b4156d44
Jay Jayjay <jayjay.jay20162016@gmail.com>
2021-03-28 09:10:43 -0400

    show scenarios option

--- source/menu/ScenarioSelectionMenu.gd
+++ source/menu/ScenarioSelectionMenu.gd
@@ -6,12 +6,12 @@ onready var buttons := $CenterContainer/VBoxContainer
 func _ready() -> void:
 	for key in Data.scenarios:
 		var scenario : ScenarioData = Data.scenarios[key]
-
-		var button = Button.new()
-		button.rect_min_size = Vector2(200, 60)
-		button.connect("pressed", self, "_on_Scenario_selected", [ scenario ])
-		button.text = scenario.alias
-		buttons.add_child(button)
+		if scenario.show:
+			var button = Button.new()
+			button.rect_min_size = Vector2(200, 60)
+			button.connect("pressed", self, "_on_Scenario_selected", [ scenario ])
+			button.text = scenario.alias
+			buttons.add_child(button)
 
 
 func _on_Scenario_selected(scenario: ScenarioData) -> void:
